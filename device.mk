c5be6f8cd7e63e7d24506fd57a250d34a8ec26b1
fOmey <pauly.galea@gmail.com>
2017-09-10 11:34:00 +1000

    Initial commit

---
+++ device.mk
@@ -0,0 +1,8 @@
+# This file includes all definitions that apply to ALL find7a devices, and
+# are also specific to find7a devices
+#
+# Everything in this directory will become public
+
+LOCAL_PATH := device/doogee/mix
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
---
+++ omni_mix.mk
@@ -0,0 +1,25 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product-if-exists, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/doogee/mix/doogee-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/doogee/mix/overlay
+
+LOCAL_PATH := device/doogee/mix
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := omni_mix
+PRODUCT_DEVICE := mix
