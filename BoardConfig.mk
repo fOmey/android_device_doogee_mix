22191935cb682c90c8e95da3a2ba0f85aee8c8ef
fOmey <pauly.galea@gmail.com>
2017-09-10 16:10:21 +1000

    Few config fixes, errors still firing in recovery.log tho

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,7 +66,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/etc/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/etc/twrp.fstab
+
 # TWRP-specific
 DEVICE_RESOLUTION := 1080x1920
 DEVICE_SCREEN_WIDTH := 1080
@@ -74,36 +75,38 @@ DEVICE_SCREEN_HEIGHT := 1920
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
-TW_NO_REBOOT_BOOTLOADER := true
+#TW_NO_REBOOT_BOOTLOADER := true
 TW_THEME := portrait_hdpi
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
-TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_MAX_BRIGHTNESS := 255
-BOARD_SUPPRESS_SECURE_ERASE := true
-TW_INCLUDE_CRYPTO := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_MAX_BRIGHTNESS := 255
-TW_NO_USB_STORAGE := true
-BOARD_USE_FRAMEBUFFER_ALPHA_CHANNEL := true
-TARGET_DISABLE_TRIPLE_BUFFERING := false
-TW_USE_TOOLBOX := false
+#TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+#RECOVERY_GRAPHICS_USE_LINELENGTH := true
+#TW_MAX_BRIGHTNESS := 255
+#BOARD_SUPPRESS_SECURE_ERASE := true
+#TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+#TW_MAX_BRIGHTNESS := 255
+#TW_NO_USB_STORAGE := true
+#BOARD_USE_FRAMEBUFFER_ALPHA_CHANNEL := true
+#TARGET_DISABLE_TRIPLE_BUFFERING := false
+#TW_USE_TOOLBOX := false
 RECOVERY_SDCARD_ON_DATA := true
 TW_INTERNAL_STORAGE_PATH := "/data/media"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_INCLUDE_CRYPTO := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/mtk-msdc.0/11120000.msdc0/by-name/userdata"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+#TW_INCLUDE_CRYPTO := true
+#TW_CRYPTO_FS_TYPE := "ext4"
+#TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/mtk-msdc.0/11120000.msdc0/by-name/userdata"
+#TW_CRYPTO_MNT_POINT := "/data"
+#TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+BOARD_HAS_FLIPPED_SCREEN := true
+#RECOVERY_GRAPHICS_FORCE_SINGLE_BUFFER := true
+
 # CWM
-#BOARD_RECOVERY_SWIPE := true
-#BOARD_SUPPRESS_EMMC_WIPE := true
-#BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-#TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+BOARD_RECOVERY_SWIPE := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
